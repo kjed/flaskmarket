05783be7c49f44d14cea2a808c8c0b8397bf5ee9
Krzysztof Jedrzejewski <krzysztof.jedrzejewski.gd@gmail.com>
2023-02-21 17:04:05 +0100

    add form validators

--- market/routes.py
+++ market/routes.py
@@ -26,4 +26,7 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
+    if form.errors != {}:  # If there are no errors from the validations
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
     return render_template('register.html', form=form)
