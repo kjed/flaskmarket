4ac8d1e02d66fbbe1277e3e87f8ed3a6d25cec6d
Krzysztof Jedrzejewski <krzysztof.jedrzejewski.gd@gmail.com>
2023-02-23 15:54:55 +0100

    add Bcrypt

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '650baa764260562acfcb16b2'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
